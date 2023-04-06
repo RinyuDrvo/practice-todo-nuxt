1a61816c76499baa132caa21d599c5b03a39ca90
RinyuDrvo <alchemist.drumvocal@gmail.com>
2023-04-06 16:50:23 +0900

    gitignoreにnode_modules追記

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/hayashiyuu/project/practice-todo-nuxt/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default /* #__PURE__ */ defuFn(inlineConfig)
---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///Users/hayashiyuu/project/practice-todo-nuxt/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
