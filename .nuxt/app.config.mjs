f00e05da0dccfe288654a3d8b1351e89b488cc4b
RinyuDrvo <alchemist.drumvocal@gmail.com>
2023-04-15 18:35:06 +0900

    gitignoreにnuxtファイルを含める

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/Users/hayashiyuu/project/practice-todo-nuxt/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default /* #__PURE__ */ defuFn(inlineConfig)
--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/hayashiyuu/project/practice-todo-nuxt/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
