12b17f8e78c6c80885852edd8194227de7c49b30
RinyuDrvo <alchemist.drumvocal@gmail.com>
2023-04-15 12:22:43 +0900

    カスタムエラーページ作成

---
+++ error.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex h-screen items-center justify-center bg-gray-200">
+    <div
+      class="bg-white p-8 border-gray-300 border rounded-md shadow-lg max-w-xl w-full"
+    >
+      <div class="text-center">
+        <p class="mt-2 text-sm text-gray-500">{{ error?.message }}</p>
+        <div class="mt-6">
+          <nuxt-link
+            to="/"
+            class="text-base font-medium text-red-600 hover:text-red-500"
+          >
+            ホームに戻る
+          </nuxt-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const error = useError();
+</script>
